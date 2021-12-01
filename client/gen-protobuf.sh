6f172967de411e4464902ffae245dcb2ae25923a
logica <84759675+logica0419@users.noreply.github.com>
2021-12-01 20:07:20 +0900

    updated protoc generation for client

--- client/gen-protobuf.sh
+++ client/gen-protobuf.sh
@@ -1,10 +1,17 @@
 #!/bin/bash
 
-sourceDir="../schema/pb"
+# .proto ファイルが入っている GitHub 上のフォルダ
+sourceDir="https://github.com/hackathon-21winter-05/oauth_ws_practice/tree/main/schema/pb"
+# .proto ファイルたちのダウンロード先 (後で削除されます)
+tempDir="./pb"
+# 生成したいコードのフォルダ
 distDir="./src/pb"
 
-mapfile -t folders < <(find "${sourceDir}" -type d -printf "%P\\n")
-mapfile -t schemes < <(find "${sourceDir}" -type f -name "*.proto" -printf "%P\\n")
+dlDir="${sourceDir/"tree/main"/"trunk"}"
+svn export ${dlDir} ${tempDir} --force
+
+mapfile -t folders < <(find "${tempDir}" -type d -printf "%P\\n")
+mapfile -t schemes < <(find "${tempDir}" -type f -name "*.proto" -printf "%P\\n")
 
 for folder in "${folders[@]}"; do
   if [ "${folder}" != "" ]; then
@@ -14,6 +21,8 @@ done
 
 for scheme in "${schemes[@]}"; do
   parsedScheme=$(echo "${scheme}" | cut -f 1 -d ".")
-  npx pbjs -t static-module -w es6 -o "${distDir}/${parsedScheme}".js "${sourceDir}/${parsedScheme}".proto
+  npx pbjs -t static-module -w es6 -o "${distDir}/${parsedScheme}".js "${tempDir}/${parsedScheme}".proto
   npx pbts -o "${distDir}/${parsedScheme}".d.ts "${distDir}/${parsedScheme}".js
 done
+
+rm -rf "${tempDir}"
